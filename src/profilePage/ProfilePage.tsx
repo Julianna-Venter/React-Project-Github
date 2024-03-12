e9829333dbfa6dc6ec24660b4cefca58610294df
juliannaventer <juliannaventer@gmail.com>
2024-03-12 12:14:09 +0200

    feat: :sparkles: Separated the background into its own component, added profile comonent

---
+++ src/profilePage/ProfilePage.tsx
@@ -0,0 +1,11 @@
+import BackgroundAssets from "../commonComponents/BackgroundAssets";
+
+function ProfilePage() {
+  return (
+    <div>
+      <BackgroundAssets />
+    </div>
+  );
+}
+
+export default ProfilePage;
