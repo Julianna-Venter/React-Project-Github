5597c493aaeb564ad5b77c6ae2e7e0f01aeea210
juliannaventer <juliannaventer@gmail.com>
2024-04-02 11:58:53 +0200

    feat: :lipstick: padding

--- src/profilePage/ProfilePage.tsx
+++ src/profilePage/ProfilePage.tsx
@@ -257,7 +257,7 @@ function ProfilePage() {
         id="outerProfileContainer"
       >
         <div
-          className="lg:h-full lg:w-11/12 lg:flex lg:flex-col lg:backdrop-blur-lg lg:bg-off-white/30 lg:rounded-2xl lg:shadow-3xl lg:px-8 lg:py-7 lg:flex-grow lg:overflow-y-scroll lg:no-scrollbar lg:m-5 lg:items-center lg:gap-5"
+          className="lg:h-full lg:w-11/12 lg:flex lg:flex-col lg:backdrop-blur-lg lg:bg-off-white/30 lg:rounded-2xl lg:shadow-3xl lg:px-8 xl:px-32 lg:py-7 lg:flex-grow lg:overflow-y-scroll lg:no-scrollbar lg:m-5 lg:items-center lg:gap-5"
           id="InnerProfileContainer"
         >
           {repoData && commitsNumber && profileData ? (
