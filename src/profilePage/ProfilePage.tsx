fe3bb084e700df25232de1483dc415a82a6b0d78
juliannaventer <juliannaventer@gmail.com>
2024-04-02 08:26:37 +0200

    Changed individual loaders to single component

--- src/profilePage/ProfilePage.tsx
+++ src/profilePage/ProfilePage.tsx
@@ -43,18 +43,15 @@ function ProfilePage() {
   const profileName = profileId;
 
   //tanstack/react-query hook to fetch the users
-  const { data: profileData, isError: isProfileError } = useQuery({
+  const {
+    data: profileData,
+    isError: isProfileError,
+    isPending,
+    isLoading,
+  } = useQuery({
     queryKey: [profileId],
     queryFn: () => getProfile(profileName),
   });
-
-  //This does not work I will have to ask for advice on how to properly implement this
-  //problem: still routes when loader is fetched (I know why, I don't know how to fix it)
-  // if (!profileData && (isPending || isLoading)) {
-  //   setTimeout(function () {
-  //     navigate({ to: "/noData" });
-  //   }, 10000);
-  // }
 
   let { data: repoData, isError: isReposError } = useQuery({
     queryKey: ["Repos", profileId],
@@ -203,6 +200,27 @@ function ProfilePage() {
       return 0;
     }
   }
+  //This does not work I will have to ask for advice on how to properly implement this
+  //problem: still routes when loader is fetched (I know why, I don't know how to fix it)
+  // let timerId: NodeJS.Timeout | null = null;
+  // if (!profileData && (isPending || isLoading)) {
+  //   console.log("Loading...");
+  //   timerId = setTimeout(function () {
+  //     if (
+  //       profileData &&
+  //       repoData &&
+  //       starsData &&
+  //       commitsNumber &&
+  //       languagesQueriesData &&
+  //       timerId
+  //     ) {
+  //       console.log("Data loaded successfully");
+  //       clearTimeout(timerId);
+  //     } else {
+  //       navigate({ to: "/noData" });
+  //     }
+  //   }, 5000);
+  // }
 
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center p-5 gap-5 lg:p-0">
@@ -215,106 +233,108 @@ function ProfilePage() {
           className="lg:h-full lg:w-11/12 lg:flex lg:flex-col lg:backdrop-blur-lg lg:bg-off-white/30 lg:rounded-2xl lg:shadow-3xl lg:px-8 lg:py-7 lg:flex-grow lg:overflow-y-scroll lg:no-scrollbar lg:m-5 lg:items-center lg:gap-5"
           id="InnerProfileContainer"
         >
-          {repoData && commitsNumber ? (
-            <div
-              id="headerContainer"
-              className="flex flex-col gap-5 lg:flex-row lg:justify-center lg:items-center lg:gap-10"
-            >
-              <div className="w-full pb-10 pt-8 self-start flex justify-center items-center gap-5 relative lg:w-fit lg:justify-start">
-                <div className="avatar absolute mr-20 lg:mr-0">
-                  <div className="w-48 h-48 rounded-full shadow-3xl lg:w-60 lg:h-60">
-                    {profileData?.avatar_url ? (
-                      <img src={profileData?.avatar_url} alt="Profile Avatar" />
-                    ) : (
-                      <img
-                        src="https://i0.wp.com/sunrisedaycamp.org/wp-content/uploads/2020/10/placeholder.png?ssl=1"
-                        alt="Profile Avatar"
-                      />
-                    )}
-                  </div>
-                </div>
-                <QuickStats
-                  starsNumber={starsData}
-                  repoNumber={repoData?.length}
-                  commits={commitsNumber}
-                />
-              </div>
-              <div className="flex flex-col gap-5">
-                <div className="flex flex-col">
-                  <label className="font-bold text-dark-text text-xl">
-                    {profileData?.name}
-                  </label>
-                  <label className="text-lighter-text text-lg">
-                    {profileData?.login}
-                  </label>
-                  <p className="text-dark-text text-sm mt-2">
-                    {profileData?.bio}
-                  </p>
-                </div>
-                <StatsCarousel
-                  followers={profileData?.followers}
-                  following={profileData?.following}
-                />
-              </div>
-            </div>
-          ) : (
-            <HeaderLoader />
-          )}
-          <div className="detailsContainer w-full flex flex-col gap-5 mt-4 lg:w-5/6">
-            {languagesQueriesData.length != 0 && languagesQueries ? (
-              <StatsRadial result={languagesQueriesData} />
-            ) : (
-              <LanguagesLoader />
-            )}
-            <div className="flex flex-col m-5 lg:px-20">
-              {Object.keys(commitsData).length != 0 ? (
-                <>
-                  <Calendar
-                    values={commitsData}
-                    until={until}
-                    weekLabelAttributes={weekLabelAttributes}
-                    monthLabelAttributes={monthLabelAttributes}
-                    panelAttributes={panelAttributes}
-                    panelColors={panelColors}
-                  />
-                  <div className="flex gap-5 items-center text-dark-text text-sm self-center">
-                    <span>Less</span>
-                    <div className="flex gap-2">
-                      {panelColors.map((color) => (
-                        <div
-                          key={color}
-                          className="w-2.5 h-2.5 rounded-sm"
-                          style={{
-                            backgroundColor: color,
-                          }}
-                        ></div>
-                      ))}
+          {repoData && commitsNumber && profileData ? (
+            <>
+              <div
+                id="headerContainer"
+                className="flex flex-col gap-5 lg:flex-row lg:justify-center lg:items-center lg:gap-10"
+              >
+                <div className="w-full pb-10 pt-8 self-start flex justify-center items-center gap-5 relative lg:w-fit lg:justify-start">
+                  <div className="avatar absolute mr-20 lg:mr-0">
+                    <div className="w-48 h-48 rounded-full shadow-3xl lg:w-60 lg:h-60">
+                      {profileData?.avatar_url ? (
+                        <img
+                          src={profileData?.avatar_url}
+                          alt="Profile Avatar"
+                        />
+                      ) : (
+                        <img
+                          src="https://i0.wp.com/sunrisedaycamp.org/wp-content/uploads/2020/10/placeholder.png?ssl=1"
+                          alt="Profile Avatar"
+                        />
+                      )}
                     </div>
-                    <span>More</span>
                   </div>
-                </>
-              ) : (
-                <CalendarLoader />
-              )}
-            </div>
-
-            <div className="flex flex-col gap-5 lg:px-20">
-              {repoData ? (
-                Object.values(repoData).map((repo: RepoItem) => (
-                  <RepoCard
-                    key={repo.id}
-                    repoName={repo.name}
-                    repoInfo={repo}
-                    branchNumber={returnBranches(repo.name)}
-                    languageData={returnLanguages(repo.name)}
-                    profileId={profileId}
+                  <QuickStats
+                    starsNumber={starsData}
+                    repoNumber={repoData?.length}
+                    commits={commitsNumber}
                   />
-                ))
-              ) : (
-                <RepoCardLoader />
-              )}
+                </div>
+                <div className="flex flex-col gap-5">
+                  <div className="flex flex-col">
+                    <label className="font-bold text-dark-text text-xl">
+                      {profileData?.name}
+                    </label>
+                    <label className="text-lighter-text text-lg">
+                      {profileData?.login}
+                    </label>
+                    <p className="text-dark-text text-sm mt-2">
+                      {profileData?.bio}
+                    </p>
+                  </div>
+                  <StatsCarousel
+                    followers={profileData?.followers}
+                    following={profileData?.following}
+                  />
+                </div>
+              </div>
+
+              <div className="detailsContainer w-full flex flex-col gap-5 mt-4 lg:w-5/6">
+                {languagesQueriesData.length != 0 && languagesQueries && (
+                  <StatsRadial result={languagesQueriesData} />
+                )}
+                <div className="flex flex-col m-5 lg:px-20">
+                  {Object.keys(commitsData).length != 0 && (
+                    <>
+                      <Calendar
+                        values={commitsData}
+                        until={until}
+                        weekLabelAttributes={weekLabelAttributes}
+                        monthLabelAttributes={monthLabelAttributes}
+                        panelAttributes={panelAttributes}
+                        panelColors={panelColors}
+                      />
+                      <div className="flex gap-5 items-center text-dark-text text-sm self-center">
+                        <span>Less</span>
+                        <div className="flex gap-2">
+                          {panelColors.map((color) => (
+                            <div
+                              key={color}
+                              className="w-2.5 h-2.5 rounded-sm"
+                              style={{
+                                backgroundColor: color,
+                              }}
+                            ></div>
+                          ))}
+                        </div>
+                        <span>More</span>
+                      </div>
+                    </>
+                  )}
+                </div>
+
+                <div className="flex flex-col gap-5 lg:px-20">
+                  {repoData &&
+                    Object.values(repoData).map((repo: RepoItem) => (
+                      <RepoCard
+                        key={repo.id}
+                        repoName={repo.name}
+                        repoInfo={repo}
+                        branchNumber={returnBranches(repo.name)}
+                        languageData={returnLanguages(repo.name)}
+                        profileId={profileId}
+                      />
+                    ))}
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="flex flex-col justify-center gap-10">
+              <HeaderLoader /> <LanguagesLoader /> <CalendarLoader />{" "}
+              <RepoCardLoader />
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
