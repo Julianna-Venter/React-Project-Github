b3eae7bab11152370c5c1aa9dc8b4bb20fad6c6b
juliannaventer <juliannaventer@gmail.com>
2024-05-16 14:19:02 +0200

    feat: :sparkles: commitItem display on mobile

--- src/edgeCases/Loaders.tsx
+++ src/edgeCases/Loaders.tsx
@@ -76,12 +76,7 @@ export function CalendarLoader() {
 }
 
 export function RepoCardLoader() {
-  return (
-    <div
-      className="skeleton w-full
-   lg:w-[34.2rem] h-[11.4rem]"
-    ></div>
-  );
+  return <div className="skeleton w-full lg:w-[34.2rem] h-[11.4rem]"></div>;
 }
 
 export function RepoPageLoader() {
